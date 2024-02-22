e55d7545ada504d847d03621510d532247348f6f
M7hammedEmad <mohamed.emad@joovlly.com>
2024-02-22 16:33:12 +0200

    implement getTitles() method in MovieController and its test

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -2,15 +2,42 @@
 
 namespace App\Http\Controllers;
 
+use App\Exceptions\MovieServiceUnavailableException;
+use App\Services\MovieService;
+use Exception;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
+use Psr\Container\ContainerExceptionInterface;
+use Psr\Container\NotFoundExceptionInterface;
 
 class MovieController extends Controller
 {
-    public function getTitles(Request $request): JsonResponse
-    {
-        // TODO
+    /**
+     * @var MovieService
+     */
+    private MovieService $movieService;
 
-        return response()->json([]);
+    /**
+     * MovieController constructor.
+     * @param MovieService $movieService
+     */
+    public function __construct(MovieService $movieService)
+    {
+        $this->movieService = $movieService;
+    }
+
+    /**
+     * @return JsonResponse
+     * @throws ContainerExceptionInterface
+     * @throws NotFoundExceptionInterface
+     * @throws MovieServiceUnavailableException
+     */
+    public function getTitles(): JsonResponse
+    {
+        try {
+            $titles = $this->movieService->getTitles();
+            return response()->json($titles);
+        } catch (Exception) {
+            return response()->json(['status' => 'failure'], 500);
+        }
     }
 }
